d2c19a7a0c28551d47c70728d94880ec4841b501
douglasew <douglas_ewerton10@hotmail.com>
2022-06-28 19:11:54 -0300

    add github link

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
-//import { Avatar, Image } from 'react-native-elements'
-//import { Image } from 'react-native-elements'
+import { Image, Linking, StyleSheet, Text, View } from 'react-native'
 import logo from '../../assets/images/Black__Yellow_Museum_Logo.png'
 import Toolbar from '../../components/toolbar'
 
@@ -21,9 +19,16 @@ const About = (props: AboutProps) => {
             }}
             style={{ width: 200, height: 200 }}
           />
-
-          <Text>1.0.0</Text>
-          <Text>app desenvolvido por: Douglas Ewerton</Text>
+          <Text>1.0.5</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text>App desenvolvido por: </Text>
+            <Text
+              style={{ color: 'blue', fontWeight: 'bold' }}
+              onPress={() => Linking.openURL('https://github.com/douglasew')}
+            >
+              @douglasew
+            </Text>
+          </View>
         </View>
       </View>
     </>
