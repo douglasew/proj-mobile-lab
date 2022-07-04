4cca21be1c03f7456a6938566d3f6d3de445f9c8
douglasew <douglas_ewerton10@hotmail.com>
2022-07-04 00:21:33 -0300

    import gesture handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import Navigation from './src/navigation'
 
+import {} from 'react-native-gesture-handler'
+
 export default function App() {
-  return <Navigation />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Navigation />
+    </GestureHandlerRootView>
+  )
 }
